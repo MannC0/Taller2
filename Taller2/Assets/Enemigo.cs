52c9649e0a1ef113cca760be04ccba5ecf346f95
Dario Saenz <saenzdario004@gmail.com>
2024-08-03 16:09:36 -0600

    Test Jefe

--- Taller2/Assets/Enemigo.cs
+++ Taller2/Assets/Enemigo.cs
@@ -6,11 +6,12 @@ public class Enemigo : MonoBehaviour
 {
     private GameObject jugador;
     public Rigidbody2D rigidbody;
-    // El [] es para que sea un array de cosas
     public GameObject[] rupiasPrefabs;
     public GameObject[] powerUpPrefabs;
-    public float velocidadMovimiento;
+    public float velocidadMovimiento = 5f;
     public int vida = 5;
+    public bool jefe;
+    private Vector2 direccion;
 
     private void Start()
     {
@@ -19,59 +20,64 @@ public class Enemigo : MonoBehaviour
 
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, jugador.transform.position, velocidadMovimiento * Time.deltaTime);
+        if (jugador != null)
+        {
+            int ajuste = 0;
+            direccion = (jugador.transform.position - transform.position).normalized;
+            float angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
+
+            if (jefe)
+            {
+                ajuste = 90;
+            }
+
+            rigidbody.rotation = angulo + ajuste;
+
+            // Move the enemy towards the player
+            rigidbody.velocity = direccion * velocidadMovimiento;
+        }
     }
 
     public void BajarVida()
     {
-        vida = vida - 1;
-        if (vida == 0)
+        vida -= 1;
+        if (vida <= 0)
         {
             MatarEnemigo();
         }
-
     }
 
     void MatarEnemigo()
     {
-        // Esto genera otro numero de 0 - 100
         int numeroAleatorioEfecto = Random.Range(0, 100);
 
-        // Esto indica que si el numero que se genera es mayor a 40, se corre el codigo de las rupias.
         if (numeroAleatorioEfecto > 40)
         {
-            // Se genera un numero del 0 - 100
             int numeroAleatorioRupia = Random.Range(0, 100);
             int numeroGenerar = 0;
 
-            // Si es menor a 65 sale una rupia verde
             if (numeroAleatorioRupia < 65)
             {
                 numeroGenerar = 0;
             }
+            else if (numeroAleatorioRupia < 95)
+            {
+                numeroGenerar = 1;
+            }
             else
             {
-                // Si es menor a 95 sale Azul
-                if (numeroAleatorioRupia < 95)
-                {
-                    numeroGenerar = 1;
-                }
-                // Si es mayor a 95 sale gris
-                else
-                {
-                    numeroGenerar = 2;
-                }
+                numeroGenerar = 2;
             }
-            // Esto genera el numero que corresponde a cada rupia (1,2,3), estas posiciones corresponden a las que tiene Unity en el inspector mode, estan en el mismo orden que fueron puestas ahi
+
             GameObject rupia = Instantiate(rupiasPrefabs[numeroGenerar], transform.position, Quaternion.identity);
             Destroy(rupia, 5f);
         }
         else
         {
-            // En este caso dice [0] porque solo hay 1 item de PowerUp.
-            GameObject PowerUp = Instantiate(powerUpPrefabs[0], transform.position, Quaternion.identity);
-            Destroy(PowerUp, 5f);
+            GameObject powerUp = Instantiate(powerUpPrefabs[0], transform.position, Quaternion.identity);
+            Destroy(powerUp, 5f);
         }
+
         Destroy(gameObject);
     }
 }
